38b5aac576441c390166de4463fd1a6923ff81a4
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2026-01-30 23:32:57 -0500

    Add internal ChunkAsyncV2 to test improved performance
    Added internal ChunkAsyncV2<T> extension method to split IAsyncEnumerable<T> into fixed-size chunks using arrays, with cancellation and argument validation. Exposed internals to the Benchmarks project via InternalsVisibleTo for testing and benchmarking.

--- src/Wolfgang.Extensions.IAsyncEnumerable/IAsyncEnumerableExtensions.cs
+++ src/Wolfgang.Extensions.IAsyncEnumerable/IAsyncEnumerableExtensions.cs
@@ -4,6 +4,8 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("Wolfgang.Extensions.IAsyncEnumerable.Benchmarks")]
+
 namespace Wolfgang.Extensions.IAsyncEnumerable;
 
 /// <summary>
@@ -62,4 +64,68 @@ public static class IAsyncEnumerableExtensions
 			yield return chunk;
 		}
 	}
+
+    
+
+    /// <summary>
+    /// Splits an IAsyncEnumerable{T} into chunks of a specified maximum size.
+    /// </summary>
+    /// <param name="source">The source IAsyncEnumerable{T} to chunk.</param>
+    /// <param name="maxChunkSize">The maximum size of each chunk.</param>
+    /// <param name="token">A cancellation token to cancel the operation.</param>
+    /// <typeparam name="T">The type of elements in the IAsyncEnumerable{T}.</typeparam>
+    /// <returns>An IAsyncEnumerable{ICollection{T}} representing the chunks.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when maxChunkSize is less than or equal to zero.</exception>
+    internal static async IAsyncEnumerable<ICollection<T>> ChunkAsyncV2<T>
+    (
+        this IAsyncEnumerable<T> source,
+        int maxChunkSize,
+        [EnumeratorCancellation] CancellationToken token = default
+    )
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        token.ThrowIfCancellationRequested();
+
+        if (maxChunkSize <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(maxChunkSize), "Chunk size must be greater than zero.");
+        }
+
+        //var chunk = new List<T>(maxChunkSize);
+        var chunk = new T[maxChunkSize];
+        var index = 0;
+        await foreach (var item in source.WithCancellation(token))
+        {
+            token.ThrowIfCancellationRequested();
+
+            chunk[index++] = item;
+            if (index < maxChunkSize)
+            {
+                continue;
+            }
+
+            yield return chunk;
+            token.ThrowIfCancellationRequested();
+            chunk = new T[maxChunkSize];
+            index = 0;
+        }
+
+        if (index == 0)
+        {
+            yield break;
+        }
+
+        token.ThrowIfCancellationRequested();
+        Array.Resize(ref chunk, index);
+        yield return chunk;
+    }
+
+
+
 }
+
+
